7d55f0acec47bbc69380fe8e16ac0533997496b8
Kapy <93550898+Kapy2003@users.noreply.github.com>
2024-08-01 14:32:42 +0530

    Update encode_all.sh

--- encode_all.sh
+++ encode_all.sh
@@ -1,10 +1,12 @@
-video_recording="/hdd/Videos/obs_recording/needs_transcode/"
-transcoded="/hdd/Videos/obs_recording/transcoded/"
+#!/bin/bash
+# from where the videos files will be pulled
+video_recording="~/Videos/"
+# the transcoded video files will be stored here
+transcoded="~/Videos/transcoded/"
 
 mkdir -p "$transcoded"
 for i in "$video_recording"*.mp4; do
   base_name=$(basename "$i" .mp4)
-  #echo "Enter a name for the encoded video for $base_name:"
-  #read -r new_name
   ffmpeg -i "$i" -vcodec mjpeg -q:v 0 -acodec pcm_s16be -q:a 0 -f mov "$transcoded${base_name}.mov"
 done
+echo "Transcoding done....."
